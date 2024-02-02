fc3e72b4685c11ef9819b13a7a2e4111fc36316f
Kendrick Joo <jtae1225@gmail.com>
2024-02-02 18:13:38 -0500

    Update main.tf
    creating temp bucket for PySpark upload to GBQ job

--- terraform/main.tf
+++ terraform/main.tf
@@ -40,6 +40,17 @@ resource "google_storage_bucket" "data-lake-bucket" {
   force_destroy = true
 }
 
+# Temporary Bucket for PySpark upload to GBQ
+# resource "google_storage_bucket" "temp-bucket" {
+#   name          = "tft_temp_bucket" # Change this to make it unique
+#   location      = var.region
+
+#   storage_class = var.storage_class
+#   uniform_bucket_level_access = true
+
+#   force_destroy = true
+# }
+
 # DWH
 # Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/bigquery_dataset
 resource "google_bigquery_dataset" "dataset" {
